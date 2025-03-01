4eddf33f3da5357e47265fa467ac866079fdd2c3
Luke Parker <lparkermg@outlook.com>
2025-03-01 21:30:59 +0000

    Moves cv stuff into CvBuilder component.

--- web/src/Components/CvBuilder/CvBuilder.tsx
+++ web/src/Components/CvBuilder/CvBuilder.tsx
@@ -1,5 +1,73 @@
+import { useEffect, useState } from "react"
+import { ThemeResponse, TemplateResponse } from "../../../models/api"
+import * as api from '../../../api/api';
+import CvSidebar from "./CvSidebar";
+import CvContentArea from "./CvContentArea";
+
 function CvBuilder(){
-    return <div />
+    const [themes, setThemes] = useState<ThemeResponse>({themes:[]})
+    const [templates, setTemplates] = useState<TemplateResponse>({templates: []})
+
+    const [content, setContent] = useState<string[]>([])
+    const [sidebar, setSidebar] = useState<string[]>([])
+
+    const [hasGenerated, setHasGenerated] = useState<boolean>(false);
+    const [pdfLink, setPdfLink] = useState<string>("")
+
+    useEffect(() => {
+    fetchAvailableDetails();
+    }, [])
+
+    async function fetchAvailableDetails(){
+        try{
+            const themeRes = await api.getThemes();
+            const templateRes = await api.getTemplates();
+
+            setThemes(themeRes);
+            setTemplates(templateRes);
+        } catch (error){
+            console.error({error, err:"failed"});
+        }
+    }
+
+    function addNewContentSection(){
+        const contentClone = [...content];
+        contentClone.push("");
+        setContent(contentClone);
+    }
+
+    function updateContentSection(index: number, newContent: string){
+        const contentClone = [...content];
+        contentClone[index] = newContent;
+        setContent(contentClone);
+    }
+
+    function addNewSidebarSection(){
+        const sidebarClone = [...sidebar];
+        sidebarClone.push("");
+        setSidebar(sidebarClone);
+    }
+
+    function updateSidebarSection(index: number, newContent: string){
+        const sidebarClone = [...sidebar];
+        sidebarClone[index] = newContent;
+        setSidebar(sidebarClone);
+    }
+
+    async function generateCv(template: number, theme: number){
+        const data = await api.postCv({ template, theme, content: content, sidebar: sidebar});
+
+        if (data){
+            setPdfLink(data);
+            setHasGenerated(true);
+        }
+    }
+    return(
+    <section>
+        <CvSidebar title="CV Settings" templates={templates.templates} themes={themes.themes} onSubmit={(template, theme) => generateCv(template, theme)} />
+        <CvContentArea />
+    </section>
+    )
 }
 
 export default CvBuilder;
