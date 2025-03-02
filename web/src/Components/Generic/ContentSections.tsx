0d7177067c64eadf43b8e6e77e01f814dc760b83
Luke Parker <lparkermg@outlook.com>
2025-03-02 16:54:21 +0000

    Adds messaging for 0 content items.

--- web/src/Components/Generic/ContentSections.tsx
+++ web/src/Components/Generic/ContentSections.tsx
@@ -8,11 +8,21 @@ interface ContentSectionsProps{
 
 function ContentSections({title, content, onContentChanged,onNewSectionAdded, onContentRemoved}: ContentSectionsProps){
 
-    const mappedContent = content.map((v,i) => <div className="content-entry">
+    const mappedContent = content.map((v,i) => <div key={i} className="content-entry">
         <button className="btn-cancel" onClick={() => onContentRemoved(i)}>Remove Section</button>
         <textarea onChange={e => onContentChanged(i, e.target.value)} value={v} />
             </div>
     );
+
+    if (mappedContent.length === 0){
+        return <div className="content-section">
+            <div className="section-header">
+                <h2>{title}</h2>
+                <button onClick={onNewSectionAdded}>+</button>
+            </div>
+            <p>No sections have been added</p>
+        </div>
+    }
 
     return <div className="content-section">
         <div className="section-header">
