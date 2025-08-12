fb12cb5b64c9ec4264a298e9383ee4397796159b
lparkermg <lparkermg@outlook.com>
2025-08-12 21:31:37 +0100

    Sets up container/dev config.

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+// This is the config that is used to run the UI from outside the container, mainly for active development.
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	define: {
+		'process.env': process.env
+	},
+	server: {
+		port: 5117
+	}
 });
