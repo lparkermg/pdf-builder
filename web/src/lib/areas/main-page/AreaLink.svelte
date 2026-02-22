a5da9744499606317c5df173a6c443b3c2821d02
lparkermg <lparkermg@outlook.com>
2026-02-22 15:48:56 +0000

    Builds main page link button.

---
+++ web/src/lib/areas/main-page/AreaLink.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { Button } from "bits-ui";
+    import type { Snippet } from "svelte";
+
+    let {
+        href,
+        children
+    }: {
+        href: string,
+        children?: Snippet
+    } = $props();
+</script>
+
+<Button.Root
+    href={href}
+    class="flex flex-col items-center justify-center gap-2 min-h-[300px] min-w-[300px] max-w-[300px] p-8 transition-colors bg-white/80 backdrop-blur-sm rounded-2xl p-6 shadow-sm border border-slate-200/50 hover:border-violet-800"
+>
+    {@render children?.()}
+</Button.Root>
