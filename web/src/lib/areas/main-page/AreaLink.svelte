d5dd9013613108aa54c7c3320485734882046a3b
lparkermg <lparkermg@outlook.com>
2026-02-22 16:02:09 +0000

    Updates main page with heading.

--- web/src/lib/areas/main-page/AreaLink.svelte
+++ web/src/lib/areas/main-page/AreaLink.svelte
@@ -13,7 +13,7 @@
 
 <Button.Root
     href={href}
-    class="flex flex-col items-center justify-center gap-2 min-h-[300px] min-w-[300px] max-w-[300px] p-8 transition-colors bg-white/80 backdrop-blur-sm rounded-2xl p-6 shadow-sm border border-slate-200/50 hover:border-violet-800"
+    class="flex flex-col items-center justify-center gap-2 min-h-[300px] min-w-[300px] max-w-[300px] p-8 transition-colors bg-white/80 backdrop-blur-sm rounded-2xl p-6 shadow-sm border border-slate-200/50 hover:border-violet-800 hover:bg-violet-100"
 >
     {@render children?.()}
 </Button.Root>
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -5,6 +5,10 @@
 
 </script>
 <Background>
+    <div class="flex flex-col items-center justify-center">
+        <h1 class="text-3xl font-bold text-slate-800">PDF Builder</h1>
+        <p class="text-md font-semibold text-slate-400">Build various types of documents</p>
+    </div>
     <div class="flex flex-col gap-2">
         <AreaLink href="/cv">
             <FileText class="w-16 h-16 text-violet-400"/>
