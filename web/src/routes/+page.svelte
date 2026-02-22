2b597e9a509c491389763e012c7b15660a14b579
Luke Parker <lparkermg@outlook.com>
2026-02-22 16:05:48 +0000

    Main Page Updates (#9)
    * Builds main page link button.
    
    * Updates main page with new button.
    
    * Updates header on CV page with button to go home.
    
    * Updates main page with heading.

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,7 +1,19 @@
 <script>
     import Background from "$lib/areas/Background.svelte";
+    import AreaLink from "$lib/areas/main-page/AreaLink.svelte";
+    import { FileText } from "lucide-svelte";
 
 </script>
 <Background>
-    <h1>Main Page</h1>
+    <div class="flex flex-col items-center justify-center">
+        <h1 class="text-3xl font-bold text-slate-800">PDF Builder</h1>
+        <p class="text-md font-semibold text-slate-400">Build various types of documents</p>
+    </div>
+    <div class="flex flex-col gap-2">
+        <AreaLink href="/cv">
+            <FileText class="w-16 h-16 text-violet-400"/>
+            <h1 class="text-lg font-bold">CV Builder</h1>
+            <p class="text-md font-semibold text-slate-400">Build CVs using markdown and generate them into PDFs.</p>
+        </AreaLink>
+    </div>
 </Background>
