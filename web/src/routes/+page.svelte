cf49aaefba450937d27c70f5df2f92fec19198eb
lparkermg <lparkermg@outlook.com>
2026-02-22 15:49:09 +0000

    Updates main page with new button.

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,7 +1,15 @@
 <script>
     import Background from "$lib/areas/Background.svelte";
+    import AreaLink from "$lib/areas/main-page/AreaLink.svelte";
+    import { FileText } from "lucide-svelte";
 
 </script>
 <Background>
-    <h1>Main Page</h1>
+    <div class="flex flex-col gap-2">
+        <AreaLink href="/cv">
+            <FileText class="w-16 h-16 text-violet-400"/>
+            <h1 class="text-lg font-bold">CV Builder</h1>
+            <p class="text-md font-semibold text-slate-400">Build CVs using markdown and generate them into PDFs.</p>
+        </AreaLink>
+    </div>
 </Background>
